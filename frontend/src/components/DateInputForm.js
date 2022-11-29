2ed9352e4487373ac10366f3db9a41a66b6b3b27
seungho <fbtmdgh4517@naver.com>
2022-11-29 21:05:55 +0900

    라벨 텍스트 변경

--- frontend/src/components/DateInputForm.js
+++ frontend/src/components/DateInputForm.js
@@ -2,7 +2,7 @@ function DateInputForm(props) {
     return (
         <div className="max-w-md container mx-auto" id={props.id}>
             <label className="text-lg font-semibold" htmlFor="when">
-                언제
+                날짜
             </label>
             <input className={props.className} type="datetime-local" name="when" id="when" ref={props.inputRef} />
         </div>
