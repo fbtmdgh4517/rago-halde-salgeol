9b47373fa1a4b0f5e24af24813d7930d24017aeb
seungho <fbtmdgh4517@naver.com>
2022-12-03 20:15:54 +0900

    ui 수정

--- frontend/src/components/auth/AuthTemplate.js
+++ frontend/src/components/auth/AuthTemplate.js
@@ -1,47 +1,15 @@
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
-import palette from '../../lib/styles/palette';
-
-const AuthTemplateBlock = styled.div`
-    position: absolute;
-    left: 0;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    background: ${palette.gray[2]};
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`;
-
-const WhiteBox = styled.div`
-    .logo-area {
-        display: block;
-        padding-bottom: 2rem;
-        text-align: center;
-        font-weight: bold;
-        letter-spacing: 2px;
-    }
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.025);
-    padding: 2rem;
-    width: 360px;
-    background: white;
-    border-radius: 2px;
-`;
 
 const AuthTemplate = (props) => {
     return (
-        <AuthTemplateBlock>
-            <WhiteBox>
-                <div className="logo-area">
-                    <Link to="/" className="title">
-                        라고 할때 살껄
-                    </Link>
+        <div className="absolute bg-blue-200 flex flex-col justify-center items-center left-0 top-0 bottom-0 right-0">
+            <div className="border border-blue-200 w-[480px] mx-auto rounded-xl shadow-md mb-9 p-8 bg-white">
+                <div className="text-center font-bold pb-8 tracking-[.25rem]">
+                    <Link to="/">라고 할때 살껄</Link>
                 </div>
                 {props.children}
-            </WhiteBox>
-        </AuthTemplateBlock>
+            </div>
+        </div>
     );
 };
 
