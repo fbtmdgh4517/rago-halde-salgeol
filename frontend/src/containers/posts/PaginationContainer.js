9ee13b06dd89841e2be13436522832b40307bb3d
seungho <fbtmdgh4517@naver.com>
2022-12-03 20:42:03 +0900

    페이지네이션 수정

--- frontend/src/containers/posts/PaginationContainer.js
+++ frontend/src/containers/posts/PaginationContainer.js
@@ -1,13 +1,20 @@
-import Pagination from '../../components/posts/Pagination';
+// import Pagination from '../../components/posts/Pagination';
+import { useState } from 'react';
+import Pagination from 'react-js-pagination';
 import { useSelector } from 'react-redux';
-import { useParams, useSearchParams } from 'react-router-dom';
 
 const PaginationContainer = () => {
-    const [searchParams] = useSearchParams();
+    // const [searchParams] = useSearchParams();
 
-    const { username } = useParams();
-    const tag = searchParams.get('tag');
-    const page = parseInt(searchParams.get('page'), 10) || 1;
+    // const { username } = useParams();
+    // const tag = searchParams.get('tag');
+    // const page = parseInt(searchParams.get('page'), 10) || 1;
+
+    const [page, setPage] = useState(1);
+
+    const pageChangeHandler = (page) => {
+        setPage(page);
+    };
 
     const { lastPage, posts, loading } = useSelector(({ posts, loading }) => ({
         lastPage: posts.lastPage,
@@ -17,7 +24,24 @@ const PaginationContainer = () => {
 
     if (!posts || loading) return null;
 
-    return <Pagination tag={tag} username={username} page={parseInt(page, 10)} lastPage={lastPage} />;
+    return (
+        <div className="mx-auto items-center flex justify-center">
+            <Pagination
+                innerClass="inline-flex items-center -space-x-px"
+                itemClassFirst="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700"
+                itemClassLast="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700"
+                itemClass="px-3 py-2 leading-tight bg-white border border-gray-300"
+                activeClass="z-10 px-3 py-2 leading-tight text-white border border-blue-300 bg-blue-500"
+                activePage={page}
+                itemsCountPerPage={10}
+                totalItemsCount={posts && posts.length}
+                pageRangeDisplayed={5}
+                prevPageText={'<'}
+                nextPageText={'>'}
+                onChange={pageChangeHandler}
+            />
+        </div>
+    );
 };
 
 export default PaginationContainer;
